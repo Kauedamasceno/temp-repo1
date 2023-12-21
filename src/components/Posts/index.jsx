9b61cbfbd1420a8ed3ce834fcb9d42fd6906ba21
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2023-12-20 23:30:17 -0300

    Kauê

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,9 @@
+import { PostCard } from '../../components/PostCard';
+
+export const Posts = ({posts}) => (
+    <div className="posts">
+        {posts.map(post => (
+            <PostCard key={post.id} title={post.title} cover={post.cover} body={post.body} id={post.id} />
+        ))}
+    </div>
+)
